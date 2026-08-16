3ec7021557eb374522d4cd9949decde5528bfd3a
thetngame-maker <thetngame@gmail.com>
2026-08-16 18:47:42 -0500

    Route discovered UGC simulator

--- apps/common/ugc_urls.py
+++ apps/common/ugc_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import ugc_intake_views, ugc_views
+from . import ugc_discovery_views, ugc_intake_views, ugc_views
 
 app_name = "ugc"
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path("", ugc_views.moderation_queue, name="moderation_queue"),
     path("new/", ugc_intake_views.manual_submission_form, name="manual_submission_form"),
     path("new/create/", ugc_views.create_manual_submission_view, name="create_manual_submission"),
+    path("discovered/new/", ugc_discovery_views.discovered_item_form, name="discovered_item_form"),
+    path("discovered/new/create/", ugc_discovery_views.create_discovered_item, name="create_discovered_item"),
     path("<uuid:submission_id>/permission/", ugc_views.update_permission_view, name="update_permission"),
     path("<uuid:submission_id>/use-in-post/", ugc_views.use_in_post_view, name="use_in_post"),
     path("<uuid:submission_id>/moderate/", ugc_views.moderate_submission_view, name="moderate"),
