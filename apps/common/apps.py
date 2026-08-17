53da63c2a62da49939e675c62efe5cc8242ba976
thetngame-maker <thetngame@gmail.com>
2026-08-16 20:00:35 -0500

    Register recurring discovery scheduler

--- apps/common/apps.py
+++ apps/common/apps.py
@@ -5,3 +5,22 @@ class CommonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.common"
     verbose_name = "Common"
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+
+        post_migrate.connect(self._register_tasks, sender=self)
+
+    @staticmethod
+    def _register_tasks(sender, **kwargs):
+        from apps.common.background import register_recurring_task
+        from apps.common.ugc_discovery_tasks import (
+            DISCOVERY_SCAN_INTERVAL_SECONDS,
+            run_due_discovery_searches,
+        )
+
+        register_recurring_task(
+            run_due_discovery_searches,
+            repeat=DISCOVERY_SCAN_INTERVAL_SECONDS,
+            verbose_name="run_due_discovery_searches",
+        )
