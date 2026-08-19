eb0704202ad8a16e45a4c8d0795f2deee6545542
thetngame-maker <thetngame@gmail.com>
2026-08-19 10:23:52 -0500

    Add mobile focused review route

--- apps/common/ugc_urls.py
+++ apps/common/ugc_urls.py
@@ -18,6 +18,7 @@ app_name = "ugc"
 
 urlpatterns = [
     path("", ugc_mobile_queue_views.moderation_queue, name="moderation_queue"),
+    path("review/<uuid:submission_id>/", ugc_mobile_queue_views.mobile_review, name="mobile_review"),
     path("new/", ugc_intake_views.manual_submission_form, name="manual_submission_form"),
     path("new/create/", ugc_views.create_manual_submission_view, name="create_manual_submission"),
     path("discovered/searches/", ugc_discovery_search_views.discovery_searches, name="discovery_searches"),
