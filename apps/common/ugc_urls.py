853ca96c8a2cb694d19658171628655bdc6807cf
thetngame-maker <thetngame@gmail.com>
2026-08-19 05:37:08 -0500

    Route moderation queue through mobile-optimized view

--- apps/common/ugc_urls.py
+++ apps/common/ugc_urls.py
@@ -10,13 +10,14 @@ from . import (
     ugc_followup_views,
     ugc_intake_views,
     ugc_location_views,
+    ugc_mobile_queue_views,
     ugc_views,
 )
 
 app_name = "ugc"
 
 urlpatterns = [
-    path("", ugc_views.moderation_queue, name="moderation_queue"),
+    path("", ugc_mobile_queue_views.moderation_queue, name="moderation_queue"),
     path("new/", ugc_intake_views.manual_submission_form, name="manual_submission_form"),
     path("new/create/", ugc_views.create_manual_submission_view, name="create_manual_submission"),
     path("discovered/searches/", ugc_discovery_search_views.discovery_searches, name="discovery_searches"),
